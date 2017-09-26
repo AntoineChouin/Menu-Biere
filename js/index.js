eb6e2e9203f9c804c55cb0119cf2699407df576e
Antoine Chouinard <antoine.brochu@me.com>
2017-09-26 18:17:45 -0400

    Les chiens
    J'ai marché en arriere de toi; je t'ai suivi jusqu'à là où ton corps m'a propulsé

---
+++ js/index.js
@@ -0,0 +1,15 @@
+//http://restaurant.lavoiemaltee.com/media/mod_catalogue/produits/9/cannette_malcommode.png
+// .container>(header.menu>h1.menu_title>lorem10)+article.post.post-menu.menu>(header.post>h3>lorem5)+section.desc.post-menu_biere>p*4>lorem10
+"use strict";
+
+Holder.run({
+  domain: "example.com",
+  themes: {
+    "simple": {
+      background: "#fff",
+      foreground: "#000",
+      size: 12
+    }
+  },
+  images: "#new"
+});
